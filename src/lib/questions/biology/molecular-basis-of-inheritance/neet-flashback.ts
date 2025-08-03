1f5757fdbebeb9ef4cb2d735b9f52184f45a35fc
Mayhem <sujeetsupremacy@gmail.com>
2025-08-03 22:27:56 +0000

    Of course. Here are all the "Topic Wise Questions" from the biology modu

---
+++ src/lib/questions/biology/molecular-basis-of-inheritance/neet-flashback.ts
@@ -0,0 +1,6 @@
+// src/lib/questions/biology/molecular-basis-of-inheritance/neet-flashback.ts
+import type { Question } from '@/lib/types';
+
+const questions: Question[] = [];
+
+export default questions;
