33149203fe23e1b934bba03e9d2801b9524b492a
Mayhem <sujeetsupremacy@gmail.com>
2025-08-10 11:40:43 +0000

    the more i text in jam night, the length of the both chat and video incr

--- src/app/jamnight/[roomId]/page.tsx
+++ src/app/jamnight/[roomId]/page.tsx
@@ -239,14 +239,12 @@ export default function JamRoomPage({ params }: { params: { roomId: string } }) 
     }
 
     return (
-        <div 
-            className="flex flex-col h-screen" 
-        >
+        <div className="flex flex-col h-screen">
             <AppHeader />
-             <main className="flex-1 container mx-auto p-4 md:p-6 lg:p-8 grid grid-rows-[1fr_auto] gap-6">
+            <main className="flex-1 container mx-auto p-4 md:p-6 lg:p-8 grid grid-rows-[minmax(0,1fr)_auto] gap-6">
                 <div className="grid lg:grid-cols-3 gap-6 min-h-0">
                     <div className="lg:col-span-2 flex flex-col gap-6">
-                         <Card className="aspect-video w-full h-full overflow-hidden">
+                        <Card className="w-full h-full aspect-video overflow-hidden">
                             <YouTube
                                 videoId={roomState.currentVideoId}
                                 onReady={onPlayerReady}
@@ -264,7 +262,7 @@ export default function JamRoomPage({ params }: { params: { roomId: string } }) 
                         </Card>
                     </div>
                     <div className="lg:col-span-1 flex flex-col min-h-0">
-                         <GroupChat
+                        <GroupChat
                             messages={chatMessages}
                             onSendMessage={handleSendMessage}
                             currentUserId={user!.uid}
