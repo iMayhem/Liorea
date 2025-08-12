13f8e77f080414a6fdda7f63147cfa7589711c7c
Mayhem <sujeetsupremacy@gmail.com>
2025-08-12 12:04:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.ts
@@ -0,0 +1,51 @@
+
+import type {NextConfig} from 'next';
+const {headers} = require('./headers');
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: headers,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
