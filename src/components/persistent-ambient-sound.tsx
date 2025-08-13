b3a5114357ab68a33b80168667a9a82faf74c9f6
Mayhem <sujeetsupremacy@gmail.com>
2025-08-13 13:47:30 +0000

    site is so slow in loading. fix these issues

--- src/components/persistent-ambient-sound.tsx
+++ src/components/persistent-ambient-sound.tsx
@@ -71,10 +71,10 @@ export function PersistentAmbientSound() {
   return (
     <>
       {/* Audio elements are kept here but hidden */}
-      <audio ref={rainAudioRef} src="https://firebasestorage.googleapis.com/v0/b/neet-trackr.firebasestorage.app/o/sounds%2Frain.mp3?alt=media&token=580e8761-eff6-428d-8e96-bfed804625d5" preload="auto"></audio>
-      <audio ref={fireAudioRef} src="https://firebasestorage.googleapis.com/v0/b/neet-trackr.firebasestorage.app/o/sounds%2Ffire.mp3?alt=media&token=5ce9d307-e724-429a-9be8-304b76de7455" preload="auto"></audio>
-      <audio ref={coffeeAudioRef} src="https://firebasestorage.googleapis.com/v0/b/neet-trackr.firebasestorage.app/o/sounds%2FCoffee%20Shop%20Ambience%20_%20Cafe%20Background%20Noise%20for%20Study%2C%20Focus%20_%20White%20Noise%2C%20%EC%B9%B4%ED%8E%98%20asmr%2C%20%EB%B0%B1%EC%83%89%EC%86%8C%EC%9D%8C.mp3?alt=media&token=113642db-60fb-4abc-84dc-a48e49fead9d" preload="auto"></audio>
-      <audio ref={oceanAudioRef} src="https://firebasestorage.googleapis.com/v0/b/neet-trackr.firebasestorage.app/o/sounds%2FSoftest%20Beach%20Sounds%20from%20the%20Tropics%20-%20Ocean%20Wave%20Sounds%20for%20Sleeping%2C%20Yoga%2C%20Meditation%2C%20Study.mp3?alt=media&token=f5e4f247-b15c-4861-8063-ab506a90b188" preload="auto"></audio>
+      <audio ref={rainAudioRef} src="https://firebasestorage.googleapis.com/v0/b/neet-trackr.firebasestorage.app/o/sounds%2Frain.mp3?alt=media&token=580e8761-eff6-428d-8e96-bfed804625d5"></audio>
+      <audio ref={fireAudioRef} src="https://firebasestorage.googleapis.com/v0/b/neet-trackr.firebasestorage.app/o/sounds%2Ffire.mp3?alt=media&token=5ce9d307-e724-429a-9be8-304b76de7455"></audio>
+      <audio ref={coffeeAudioRef} src="https://firebasestorage.googleapis.com/v0/b/neet-trackr.firebasestorage.app/o/sounds%2FCoffee%20Shop%20Ambience%20_%20Cafe%20Background%20Noise%20for%20Study%2C%20Focus%20_%20White%20Noise%2C%20%EC%B9%B4%ED%8E%98%20asmr%2C%20%EB%B0%B1%EC%83%89%EC%86%8C%EC%9D%8C.mp3?alt=media&token=113642db-60fb-4abc-84dc-a48e49fead9d"></audio>
+      <audio ref={oceanAudioRef} src="https://firebasestorage.googleapis.com/v0/b/neet-trackr.firebasestorage.app/o/sounds%2FSoftest%20Beach%20Sounds%20from%20the%20Tropics%20-%20Ocean%20Wave%20Sounds%20for%20Sleeping%2C%20Yoga%2C%20Meditation%2C%20Study.mp3?alt=media&token=f5e4f247-b15c-4861-8063-ab506a90b188"></audio>
     </>
   );
 }
